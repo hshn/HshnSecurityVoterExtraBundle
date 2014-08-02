9d9dc646ef77b423a71611b49bdfc04c5fc95a4d
Shota Hoshino <lga0503@gmail.com>
2014-08-03 02:05:49 +0900

    Add ExpressionVoterTest

---
+++ Tests/Security/Voter/ExpressionVoterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hshn\SecurityVoterExtraBundle\Tests\Security\Voter;
+
+use Hshn\SecurityVoterExtraBundle\Security\Voter\ExpressionVoter;
+use Symfony\Component\Security\Core\Authorization\ExpressionLanguage;
+
+class ExpressionVoterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function testShouldBeGranted()
+    {
+        $expressionLanguage = $this->getMockBuilder('Symfony\Component\ExpressionLanguage\ExpressionLanguage')->disableOriginalConstructor()->getMock();
+        $voter = new ExpressionVoter([], [], $expressionLanguage, $expression = 'dummy expression');
+
+        $token = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        $token
+            ->expects($this->once())
+            ->method('getUser')
+            ->will($this->returnValue($user = $this->getMock('Symfony\Component\Security\Core\User\UserInterface')));
+
+        $object = new \stdClass();
+        $attribute = 'Foo';
+
+        $expressionLanguage
+            ->expects($this->once())
+            ->method('evaluate')
+            ->with($expression, $this->callback(function (array $values) use ($token, $user, $object, $attribute) {
+
+                $this->assertSame($token, $values['token']);
+                $this->assertSame($user, $values['user']);
+                $this->assertSame($object, $values['object']);
+                $this->assertEquals($attribute, $values['attribute']);
+
+                return true;
+            }))
+            ->will($this->returnValue(true));
+
+        $this->assertTrue($voter->shouldBeGranted($token, $object, $attribute));
+    }
+}
